6e3b968bf758332f3080380cc9925a27e9a630b0
filip <filip.wielkopolan@gmail.com>
2024-03-15 17:29:37 +0100

    Added loading bar while waiting for the data server to spin up Hardcoded server URL to enable static build to work TODO: Fix the control by exception (exception thrown locally - for now I leave it, cause it works ok)

--- src/App.tsx
+++ src/App.tsx
@@ -15,23 +15,23 @@ function App() {
     useEffect(() => {
         setTotalFilter({$and: [dateFilter, locationFilter]});
     }, [locationFilter, dateFilter]);
-    const updateFilter = (key, value) => {
+    const updateFilter = (key : string, value : object) => {
         setLocationFilter(prevFilter => ({
             ...prevFilter, // Spread existing properties
             [key]: value, // Override or add new property
         }));
     };
-    const removeFilterProperty = (keyToRemove) => {
+    const removeFilterProperty = (keyToRemove : string) => {
         const newFilter = {...locationFilter};
         delete newFilter[keyToRemove];
         setLocationFilter(newFilter);
     };
-    const updateDateFilter = (dateFilter) => {
+    const updateDateFilter = (dateFilter : any) => {
         setDateFilter(dateFilter);
     };
     return (
         <ThemeProvider theme={defaultTheme}>
-                <Chart height={window.innerHeight - 125} width={window.innerWidth} filter={totalFilter}
+                <Chart height={window.innerHeight - 150} width={window.innerWidth} filter={totalFilter}
                        chartId={'060074e7-5d42-46b0-82a2-1251220eaacd'}/>
                 <FilterComponent updateFilter={updateFilter} removeFilterProperty={removeFilterProperty}/>
                 <DateSelector updateDateFilter={updateDateFilter}/>
--- src/components/FilterComponent.tsx
+++ src/components/FilterComponent.tsx
@@ -1,28 +1,42 @@
 import React, {useEffect, useState} from "react";
 import {FilterSelector} from "./FilterSelector";
+import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
+import {LinearProgress} from "@mui/material";
 
-const chartDataServiceURI = process.env.REACT_APP_CHART_DATA_URI;
+const chartDataServiceURI = process.env.REACT_APP_CHART_DATA_URI || "https://mongo-chart-fields-service.onrender.com/";
 
 function FilterComponent(props: {
-    updateFilter: (key, value) => void,
-    removeFilterProperty: (keyToRemove) => void
+    updateFilter: (key: string, value: any) => void,
+    removeFilterProperty: (keyToRemove: string) => void
 }) {
     const [arrivalCities, setArrivalCities] = useState([]);
     const [departureCities, setDepartureCities] = useState([]);
     const [countries, setCountries] = useState([]);
-
+    const [loadingFilters, setLoadingFilters] = useState(true);
     useEffect(() => {
-        fetch(chartDataServiceURI + 'fetchArrivalCities')
-            .then(response => response.json())
-            .then(data => setArrivalCities(data))
-            .catch((error) => console.log("Error fetching chart data from ", chartDataServiceURI, error));
-        fetch(chartDataServiceURI + 'fetchDepartureCities')
-            .then(response => response.json())
-            .then(data => setDepartureCities(data));
-        fetch(chartDataServiceURI + 'fetchCountries')
-            .then(response => response.json())
-            .then(data => setCountries(data));
+        const fetchData = async () => {
+            try {
+                const response = await fetch(chartDataServiceURI);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+                fetch(chartDataServiceURI + 'fetchArrivalCities')
+                    .then(response => response.json())
+                    .then(data => setArrivalCities(data))
+                    .catch((error) => console.log("Error fetching chart data from ", chartDataServiceURI, error));
+                fetch(chartDataServiceURI + 'fetchDepartureCities')
+                    .then(response => response.json())
+                    .then(data => setDepartureCities(data));
+                fetch(chartDataServiceURI + 'fetchCountries')
+                    .then(response => response.json())
+                    .then(data => setCountries(data));
+                setLoadingFilters(false);
+            } catch (error) {
+                setTimeout(fetchData, 5000); // Retry after 5 seconds
+            }
+        };
+        fetchData().then(() => console.log("Fetching data from ", chartDataServiceURI));
     }, []);
 
     const handleInputChange = (event) => {
@@ -33,14 +47,20 @@ function FilterComponent(props: {
     };
 
     return (
-        <Stack direction="row" spacing={2} sx={{pl: 2}}>
-            <FilterSelector filterLabel="Arrival City" onChange={handleInputChange} filterValues={arrivalCities}
-                            onRemoveFilter={handleRemoveClick} filterName="arrivalCity"/>
-            <FilterSelector filterLabel="Departure City" onChange={handleInputChange} filterValues={departureCities}
-                            onRemoveFilter={handleRemoveClick} filterName="departureCity"/>
-            <FilterSelector filterLabel="Country" onChange={handleInputChange} filterValues={countries}
-                            onRemoveFilter={handleRemoveClick} filterName="country"/>
-        </Stack>
+        <div>
+            { loadingFilters ? (<Box sx={{pl: 2, pr: 2}}><h5> Loading filters... </h5><LinearProgress/></Box>) : (
+                <Stack direction="row" spacing={2} sx={{pl: 2}}>
+                        <FilterSelector filterLabel="Arrival City" onChange={handleInputChange}
+                                        filterValues={arrivalCities}
+                                        onRemoveFilter={handleRemoveClick} filterName="arrivalCity"/>
+                        <FilterSelector filterLabel="Departure City" onChange={handleInputChange}
+                                        filterValues={departureCities}
+                                        onRemoveFilter={handleRemoveClick} filterName="departureCity"/>
+                        <FilterSelector filterLabel="Country" onChange={handleInputChange} filterValues={countries}
+                                        onRemoveFilter={handleRemoveClick} filterName="country"/>
+                    </Stack>
+            )}
+        </div>
     );
 }
 
